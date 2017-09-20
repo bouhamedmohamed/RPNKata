fe029dc4646644883dbef2505c3d5b4be016dd7a
mohamed <mohamed.bouhamed@lcdlv.com>
2017-09-20 15:59:22 +0200

    parse string contains only 1

--- src/test/java/domain/RPNParsorTest.java
+++ src/test/java/domain/RPNParsorTest.java
@@ -10,7 +10,7 @@ public class RPNParsorTest {
 
     @Before
     public void setUp() throws Exception {
-        rpnParsor = new RPNParsor ( );
+        rpnParsor = new RPNParsor ("");
     }
 
     @Test
