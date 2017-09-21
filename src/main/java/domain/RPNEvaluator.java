342f9aabcf3d8ad70066d69e1ebc19693617db08
mohamed <mohamed.bouhamed@lcdlv.com>
2017-09-21 16:35:29 +0200

    refacto clean code

--- src/main/java/domain/RPNEvaluator.java
+++ src/main/java/domain/RPNEvaluator.java
@@ -1,16 +1,22 @@
 package domain;
 
 public class RPNEvaluator {
+
+    public static final String ADD_OPERATOR = "+";
+    public static final String SUBTRACT_OPERATOR = "-";
+    public static final String MULTIPLICATION_OPERATOR = "*";
+    public static final String DIVIDE_OPERATION = "/";
+
     public String findOperation(String s) {
         if ( !s.isEmpty ( ) ) {
-            if ( s.equals ("+") )
-                return "+";
-            if ( s.equals ("-") )
-                return "-";
-            if ( s.equals ("*") )
-                return "*";
-            if ( s.equals ("/") )
-                return "/";
+            if ( s.equals (ADD_OPERATOR) )
+                return ADD_OPERATOR;
+            if ( s.equals (SUBTRACT_OPERATOR) )
+                return SUBTRACT_OPERATOR;
+            if ( s.equals (MULTIPLICATION_OPERATOR) )
+                return MULTIPLICATION_OPERATOR;
+            if ( s.equals (DIVIDE_OPERATION) )
+                return DIVIDE_OPERATION;
         }
         throw new NoOperationException ( );
     }
