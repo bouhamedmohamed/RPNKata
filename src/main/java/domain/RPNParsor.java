afa1951e2129d0fec42d4b445389ed2f605ab469
mohamed <mohamed.bouhamed@lcdlv.com>
2017-09-21 16:04:28 +0200

    refacto

--- src/main/java/domain/RPNParsor.java
+++ src/main/java/domain/RPNParsor.java
@@ -1,20 +1,32 @@
 package domain;
 
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class RPNParsor {
-    public List parse(String expression) {
-        if ( expression.isEmpty ( ) )
-            return Collections.EMPTY_LIST;
-        else
-            return createElements (expression);
+    List<String> expressionList;
+    private AtomicInteger indice;
 
+    public RPNParsor(String expression) {
+        this.expressionList = new ArrayList<> ( );
+        if ( !expression.isEmpty ( ) )
+            this.expressionList = createElements (expression);
+
+        this.indice = new AtomicInteger (0);
     }
 
     private List createElements(String expression) {
         String[] expressionParsed = expression.split (" ");
-        return Arrays.asList (expressionParsed);
+        expressionList.addAll (Arrays.asList (expressionParsed));
+        return expressionList;
+    }
+
+
+    public String getNext() {
+        if ( expressionList.size ( ) > indice.get ( ) )
+            return expressionList.get (indice.getAndIncrement ( ));
+        return "";
     }
 }
--- src/test/java/domain/RPNParsorTest.java
+++ src/test/java/domain/RPNParsorTest.java
@@ -9,27 +9,34 @@ import java.util.Collections;
 public class RPNParsorTest {
     @Test
     public void should_return_empty_when_we_parse_empty_input() {
-        Assert.assertEquals (Collections.EMPTY_LIST, new RPNParsor ( ).parse (""));
+        final RPNParsor expressionParsed = new RPNParsor ("");
+        Assert.assertEquals ("", expressionParsed.getNext ());
     }
 
     @Test
     public void should_return_0_when_we_parse_0() {
-        Assert.assertEquals (Arrays.asList ("0"), new RPNParsor ( ).parse ("0 "));
+        final RPNParsor expressionParsed = new RPNParsor ("0 ");
+        Assert.assertEquals ("0", expressionParsed.getNext ());
     }
 
     @Test
     public void should_return_1_when_we_parse_1() {
-        Assert.assertEquals (Arrays.asList ("1"), new RPNParsor ( ).parse ("1 "));
+        final RPNParsor expressionParsed = new RPNParsor ("1 ");
+        Assert.assertEquals ("1", expressionParsed.getNext ());
     }
 
     @Test
     public void should_return_1_0_when_we_parse_1_0() {
-        Assert.assertEquals (Arrays.asList ("1", "0"), new RPNParsor ( ).parse ("1 0 "));
+        final RPNParsor expressionParsed = new RPNParsor ("1 0 ");
+        Assert.assertEquals ("1", expressionParsed.getNext ());
+        Assert.assertEquals ("0", expressionParsed.getNext ());
     }
 
     @Test
     public void should_return_1_0_when_we_parse_1_0_plus() {
-        Assert.assertEquals (Arrays.asList ("1", "0","+"), new RPNParsor ( ).parse ("1 0 +"));
+        final RPNParsor expressionParsed = new RPNParsor ("1 0 + ");
+        Assert.assertEquals ("1", expressionParsed.getNext ());
+        Assert.assertEquals ("0", expressionParsed.getNext ());
+        Assert.assertEquals ("+", expressionParsed.getNext ());
     }
-
 }
