5884863ee767ee27de7dcc9fc46198ce98288eb2
mohamed <mohamed.bouhamed@lcdlv.com>
2017-09-22 14:47:40 +0200

    clean code

--- src/main/java/domain/RPNCalculator.java
+++ src/main/java/domain/RPNCalculator.java
@@ -7,28 +7,35 @@ public class RPNCalculator {
             throw new InvalidExpressionException ( );
 
         String element = rpnParsor.getNext ( );
-        int operandOne = 0;
-        if ( rpnParsor.isOperand (element) )
-            operandOne = Integer.parseInt (element);
+        int operandOne = getOperand (rpnParsor, element);
         String elementTwo = rpnParsor.getNext ( );
-        int operandTwo = 0;
-        if ( rpnParsor.isOperand (elementTwo) )
-            operandTwo = Integer.parseInt (elementTwo);
+        int operandTwo = getOperand (rpnParsor, elementTwo);
 
         String elementThree = rpnParsor.getNext ( );
         RPNOperator rpnOperator = null;
         if ( !rpnParsor.isOperand (elementThree) )
             rpnOperator = RPNOperator.findOperation (elementThree);
 
+        return calcul (operandOne, operandTwo, rpnOperator);
+    }
+
+    private int getOperand(RPNParsor rpnParsor, String element) {
+        int operand = 0;
+        if ( rpnParsor.isOperand (element) )
+            operand = Integer.parseInt (element);
+        return operand;
+    }
+
+    private int calcul(int operandOne, int operandTwo, RPNOperator rpnOperator) {
         if ( rpnOperator.getSymbol ( ).equals ("+") )
             return rpnOperator.add (operandOne, operandTwo);
-
         if ( rpnOperator.getSymbol ( ).equals ("-") )
             return rpnOperator.sub (operandOne, operandTwo);
         if ( rpnOperator.getSymbol ( ).equals ("*") )
             return rpnOperator.multiple (operandOne, operandTwo);
-        else
+        if ( rpnOperator.getSymbol ( ).equals ("/") )
             return rpnOperator.divided (operandOne, operandTwo);
 
+        throw new NoOperationException ( );
     }
 }
